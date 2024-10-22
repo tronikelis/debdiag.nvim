dc1a72346a78a79ff8f20b16c9c6355fb94e33bf
Tronikel <contactdonatas@gmail.com>
2024-10-22 17:47:31 +0300

    no unnecessary work

--- lua/debdiag/init.lua
+++ lua/debdiag/init.lua
@@ -15,24 +15,44 @@ function M.setup(config)
     config = config or {}
     M.config = vim.tbl_deep_extend("force", M.config, config)
 
-    local diagnostics_show = utils.debounce(function(buf)
+    ---@type table<integer, boolean?>
+    local disabled_map = {}
+
+    ---@param bufnr integer
+    local function enable_diagnostics(bufnr)
+        if disabled_map[bufnr] then
+            vim.diagnostic.enable(true, { bufnr = bufnr })
+            disabled_map[bufnr] = false
+        end
+    end
+    ---@param bufnr integer
+    local function disable_diagnostics(bufnr)
+        if not disabled_map[bufnr] then
+            vim.diagnostic.enable(false, { bufnr = bufnr })
+            disabled_map[bufnr] = true
+        end
+    end
+
+    local debounced_enable_diagnostics = utils.debounce(function(buf)
         if vim.api.nvim_buf_is_valid(buf) then
-            vim.diagnostic.enable(true, { bufnr = buf })
+            enable_diagnostics(buf)
         end
     end, M.config.ms)
 
     vim.api.nvim_create_autocmd(M.config.autocmd, {
         callback = vim.schedule_wrap(function()
             local buf = vim.api.nvim_get_current_buf()
-            vim.diagnostic.enable(false, { bufnr = buf })
-            diagnostics_show(buf)
+
+            disable_diagnostics(buf)
+            debounced_enable_diagnostics(buf)
         end),
     })
 
     if M.config.enable_leave_insert then
         vim.api.nvim_create_autocmd("InsertLeave", {
             callback = function()
-                vim.diagnostic.enable(true, { bufnr = 0 })
+                local buf = vim.api.nvim_get_current_buf()
+                enable_diagnostics(buf)
             end,
         })
     end
